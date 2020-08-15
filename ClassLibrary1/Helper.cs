71fcc42d7248f3ae42da3207bf7ee62d274017b6
Shubhangi <Shubhangi@SHUBHANGI>
2020-08-15 12:09:13 +0530

    Add project files.

---
+++ ClassLibrary1/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+   public class Helper
+    {
+        public void print()
+        {
+            Console.WriteLine("helloword!!");
+        }
+    }
+}
---
+++ VBCSHARP/Program.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VBCSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Helper h = new Helper();
+            h.print();
+            Console.ReadLine();
+            
+        }
+    }
+}
